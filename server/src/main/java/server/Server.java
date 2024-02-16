8fb1bead616add1e57e41292e6ce3b2fac99008f
Spencer Marshall <sm892@byu.edu>
2024-02-15 20:29:31 -0700

    clearGames web to get API tests working

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -3,19 +3,27 @@ import spark.*;
 
 public class Server {
 
-    public int run(int desiredPort) {
+    public int run(int desiredPort)
+    {
         Spark.port(desiredPort);
 
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
-
+        Spark.delete("/games",this::deleteAllGames);
         Spark.awaitInitialization();
         return Spark.port();
     }
 
-    public void stop() {
+    public void stop()
+    {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    private Object deleteAllGames(Request req, Response res)
+    {
+        res.status(400);
+        return ' ';
     }
 }
