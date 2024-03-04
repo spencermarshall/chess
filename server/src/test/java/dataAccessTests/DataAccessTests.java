04b735761681371eebf6421dacde65cfd1f281b7
Spencer Marshall <sm892@byu.edu>
2024-03-04 11:21:04 -0700

    added test file

---
+++ server/src/test/java/dataAccessTests/DataAccessTests.java
@@ -0,0 +1,35 @@
+package dataAccessTests;
+
+import dataAccess.DataAccessException;
+import org.junit.jupiter.api.*;
+import passoffTests.testClasses.TestException;
+import server.Server;
+
+public class DataAccessTests {
+
+
+    private static Server server;
+
+
+
+    @BeforeAll //this function is copied from the starterCode on github
+    public static void init() {
+        server = new Server();
+        var port = server.run(0);
+        System.out.println("Started test HTTP server on " + port);
+
+
+
+    }
+    @AfterAll  //this function is copied from the starterCode on github
+    public static void stop()
+    {
+        server.stop();
+    }
+    @BeforeEach //this function is copied from the starterCode on github
+    public void setup() throws TestException, DataAccessException {
+
+    }
+
+
+}
